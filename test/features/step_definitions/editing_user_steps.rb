5feab146dabe46b6955506abd97c807f480b9acf
kocolondon <kingsley@kocolondon.com>
2013-11-30 21:00:28 +0000

    Add feature steps for editing user steps

---
+++ test/features/step_definitions/editing_user_steps.rb
@@ -0,0 +1,19 @@
+Given(/^that I have a user called "(.*?)" surname "(.*?)" with username "(.*?)" and password "(.*?)"$/) do |firstname, surname, username, password|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I update firstname to "(.*?)"$/) do |firstname|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I expect a search for firstname "(.*?)" to return nil$/) do |firstname|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^If I search for firstname "(.*?)"$/) do |firstname|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should get a user with firstname "(.*?)" surname "(.*?)" with username "(.*?)"$/) do |firstname, surname, username|
+  pending # express the regexp above with the code you wish you had
+end
