5b949d42657ccdeacdbb0cebcc81d4e93d6de923
kocolondon <kingsley@kocolondon.com>
2013-12-02 23:06:15 +0000

    Add some cucumber tests for editing users

--- test/features/step_definitions/editing_user_steps.rb
+++ test/features/step_definitions/editing_user_steps.rb
@@ -1,19 +1,26 @@
+require 'user'
+
 Given(/^that I have a user called "(.*?)" surname "(.*?)" with username "(.*?)" and password "(.*?)"$/) do |firstname, surname, username, password|
-  pending # express the regexp above with the code you wish you had
+  User.create(firstname: firstname, lastname: surname, username: username, password: password)
+  @user = User.find_by(firstname: firstname)
 end
 
 When(/^I update firstname to "(.*?)"$/) do |firstname|
-  pending # express the regexp above with the code you wish you had
+  @user.firstname = firstname
+  @user.save!
 end
 
-Then(/^I expect a search for firstname "(.*?)" to return nil$/) do |firstname|
-  pending # express the regexp above with the code you wish you had
+Then(/^I expect a search for firstname "(.*?)" to return nill$/) do |firstname|
+  @search = User.find_by(firstname: firstname)
+  expect(@search).to eq(nil)
 end
 
 Then(/^If I search for firstname "(.*?)"$/) do |firstname|
-  pending # express the regexp above with the code you wish you had
+	@search = User.find_by(firstname: firstname)
 end
 
-Then(/^I should get a user with firstname "(.*?)" surname "(.*?)" with username "(.*?)"$/) do |firstname, surname, username|
-  pending # express the regexp above with the code you wish you had
+Then(/^I should get a user with firstname "(.*?)" surname "(.*?)" with username "(.*?)"$/) do |firstname, lastname, username|
+  expect(@search.firstname).to eq('susan')
+  expect(@search.lastname).to eq('adams')
+  expect(@search.username).to eq('pam')
 end
